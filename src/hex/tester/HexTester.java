446e30fbcd24a5a337cf1b72ff007daa26a09de8
Ben Stickland <benstickland@hotmail.com>
2016-10-25 14:09:52 +0100

    Tidier Code
    Tidied up the code + added comments etc..

--- src/hex/tester/HexTester.java
+++ src/hex/tester/HexTester.java
@@ -7,17 +7,20 @@ package hex.tester;
 
 /**
  *
- * @author surface
  */
 public class HexTester {
 
       /**
-       * @param args the command line arguments
+       * A small program to increment through offsets for hexadecimal inputs.
        */
       public static void main(String[] args) {
-            // TODO code application logic here
+            //hexArray stores each of the hexadecimal values from the stringArray.
             int[] hexArray;
+            char[] asciiArray;
+            int initialASCIIOffset = 30;
+            int finalASCIIOffset = 50;
             
+            //Hex strings to be analysed
             String[] stringArray = {"2D203B2237", "242F202F3C",
                         "2?3F2C3320","3B3539243E",
                         "312C292C3F","3A2A323834",
@@ -34,30 +37,30 @@ public class HexTester {
                         "373231263F","3E3D312E29"};
 
             hexArray = new int[stringArray.length * 10];
-            
-            char[] asciiArray;
             asciiArray = new char[hexArray.length/2];
             
-            for(int i = 0; i < stringArray.length*10; i ++){
-                  int StringArrayCount = (int) Math.floor(i/10);
-                  char ch = stringArray[StringArrayCount].charAt(i%10);
-                  hexArray[i] = Character.getNumericValue(ch);
-                  
-                  //System.out.println(i + " Character value =" + hexArray[i]);
+            //Populate the hexArray with the numeric hexadecimal values of each character
+            for(int i = 0; i < hexArray.length; i ++){
+                  int stringArrayCount = (int) Math.floor(i/10);
+                  char currentChar = stringArray[stringArrayCount].charAt(i%10);
+                  hexArray[i] = Character.getNumericValue(currentChar);
             } 
+             
+            //Save pairs of hexadecimal values to ASCII values
+            for (int x = 0; x < hexArray.length/2; x ++){  
+                  int ASCIIValue = hexArray[2*x]*16 + hexArray[(2*x) + 1];
+                  asciiArray[x] = (char) ASCIIValue;
+            }
             
-            for(int ASCIIShift = 30; ASCIIShift < 90; ASCIIShift ++){
-                  System.out.println("ASCII Shift =" + ASCIIShift + "\n");
-                  for (int x = 0; x < hexArray.length/2; x ++){
-                        //System.out.println("x =" + x + " Hex values =" + hexArray[2*x] + " " + hexArray[(2*x)-1]);   
-                        int ASCIIValue= hexArray[2*x]*16 + hexArray[(2*x) + 1] + ASCIIShift;
-                        asciiArray[x] = (char) ASCIIValue;
-                  }
-
-                  for(int i = 0; i < asciiArray.length; i ++){
-                        System.out.print(asciiArray[i]);
+            //Increment between the initial and final offset, and print each offset's value.
+            for(int ASCIIShift = initialASCIIOffset; ASCIIShift < finalASCIIOffset; ASCIIShift ++){
+                  System.out.print("ASCII Shift = " + ASCIIShift + "   ");
+                  
+                  for(char charASCII : asciiArray){
+                        System.out.print((char) (charASCII + ASCIIShift));
                   }
                   
+                  System.out.println("\n");
             }
       }
       
